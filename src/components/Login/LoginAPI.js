81786b51be337a9b4df66fd3d76693b1d2c0a0bb
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-09-18 12:36:18 +0300

    test build
    test build for deploy

--- src/components/Login/LoginAPI.js
+++ src/components/Login/LoginAPI.js
@@ -1,66 +1,73 @@
 import React, { useState } from "react";
-import ReactLoading from 'react-loading';
+import ReactLoading from "react-loading";
 import { baseUrl } from "../../config";
 
 //---------------------
-let flag_token = false
+let flag_token = false;
 let flag = false;
 // let myStatus = 0;
 //---------------------
 
-
 function LoginAPI(props) {
+  // console.log()
+  // alert(props.getFlagLoading)
+  const [, login_token] = useState("");
+  const [, setFlag] = useState(false);
+  // const [, setMyStatus] = useState(0)
 
-    // console.log()
-    // alert(props.getFlagLoading)
-    const [, login_token] = useState('')
-    const [, setFlag] = useState(false)
-    // const [, setMyStatus] = useState(0)
+  if (props.APIDetailsLogin.user.length > 0) {
+    const url = `${baseUrl}/wp-json/jwt-auth/v1/token/`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify({
+        username: props.APIDetailsLogin.user,
+        password: props.APIDetailsLogin.pass,
+      }),
+    })
+      .then((response) =>
+        response.status === 403
+          ? alert("Wrong username/mail or wrong Password")
+          : response.json()
+      )
 
-    if (props.APIDetailsLogin.user.length > 0) {
-
-        const url = `${baseUrl}/wp-json/jwt-auth/v1/token/`
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json',
-            },
-            body: JSON.stringify({
-                username: props.APIDetailsLogin.user,
-                password: props.APIDetailsLogin.pass
-            })
-        })
-            .then((response) =>
-                response.status === 403 ? (alert('Wrong username/mail or wrong Password')) : response.json())
-
-            .then(function (user) {
-                if (!flag_token) {
-                    if (user.message !== undefined) {
-                        if (user.message.includes("2FA")) {
-                            // console.log("2FA")
-                            alert('2FA is activated, No support for this feature, Please login with another user')
-                            login_token(flag_token = true)
-                        }
-                    }
-                    setFlag(flag = true);
-                    // console.log("token", user.token)
-                    sessionStorage.setItem('jwt', user.token)
-                    sessionStorage.setItem('logged_in', 1)
-                    window.location.replace('/Dashboard')
-
-
-                }
-            })
-    }
-    return (
-        <>
-            {props.getFlagLoading && flag ? <>
-                <h1 style={{ textAlign: "center", color: "white" }}>Loading</h1>
-                < ReactLoading type={"bars"} className='loading' color={"rgb(180, 175, 199)"} height={'10%'} width={'10%'} />
-            </> : null
+      .then(function (user) {
+        if (!flag_token) {
+          if (user.message !== undefined) {
+            if (user.message.includes("2FA")) {
+              // console.log("2FA")
+              alert(
+                "2FA is activated, No support for this feature, Please login with another user"
+              );
+              login_token((flag_token = true));
             }
+          }
+          setFlag((flag = true));
+          // console.log("token", user.token)
+          sessionStorage.setItem("jwt", user.token);
+          sessionStorage.setItem("logged_in", 1);
+          window.location.replace("/Dashboard");
+        }
+      });
+  }
+  return (
+    <>
+      {props.getFlagLoading && flag ? (
+        <>
+          <h1 style={{ textAlign: "center", color: "white" }}>Loading</h1>
+          <ReactLoading
+            type={"bars"}
+            className="loading"
+            color={"rgb(180, 175, 199)"}
+            height={"10%"}
+            width={"10%"}
+          />
         </>
-    )
+      ) : null}
+    </>
+  );
 }
-export default LoginAPI
+export default LoginAPI;
