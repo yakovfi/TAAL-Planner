0daa003c06eb8ba8fdf91f5ebc8b279e6665c073
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-09-21 12:33:15 +0300

    upgrade node

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,42 +1,42 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { FcPlus, FcCalculator } from "react-icons/fc";
 
 import { RiHome4Line } from "react-icons/ri";
 
-import './style.css';
+import "./style.css";
 import { FaUser, FaAddressCard, FaRoute } from "react-icons/fa";
-import { useState } from 'react';
+import { useState } from "react";
 import { baseUrl } from "../../config";
 
-let flag_token = false
+let flag_token = false;
 
 const Nav = () => {
-    const [, login_token] = useState('')
-    const [complete_name, setcomplete_name] = useState('')
-    useEffect(() => {
-        const url2 = `${baseUrl}/wp-json/wp/v2/users/me/`
-        fetch(url2, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json',
-                'Authorization': 'Bearer' + sessionStorage.jwt
-            },
-        })
-            .then((response) => response.json())
-            .then(function (user) {
-                if (!flag_token) {
-                    login_token(flag_token = true)
-                    // console.log("user: ", user)
-                    setcomplete_name(user.name)
-                }
-            });
+  const [, login_token] = useState("");
+  const [complete_name, setcomplete_name] = useState("");
+  useEffect(() => {
+    const url2 = `${baseUrl}/wp-json/wp/v2/users/me/`;
+    fetch(url2, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+        Authorization: "Bearer" + sessionStorage.jwt,
+      },
     })
-    return (
-        <div className='nav'>
-            <ul className="nav-links">
-                {/* <Link to="/Calculator" className='link'>
+      .then((response) => response.json())
+      .then(function (user) {
+        if (!flag_token) {
+          login_token((flag_token = true));
+          // console.log("user: ", user)
+          setcomplete_name(user.name);
+        }
+      });
+  });
+  return (
+    <div className="nav">
+      <ul className="nav-links">
+        {/* <Link to="/Calculator" className='link'>
                     <li><FcCalculator style={{ fontSize: '24px' }} />&nbsp;&nbsp; פעולות נוספות</li>
                 </Link>
                 <Link to="/student" className='link'>
@@ -48,20 +48,14 @@ const Nav = () => {
                 <Link to="/planner" className='link'>
                     <li><FcPlus style={{ fontSize: '24px' }} /> &nbsp;&nbsp;הוסף מסלול </li>
                 </Link> */}
-                <Link
-                    to="/Dashboard"
-                >
-                    <div className='home'></div>
-                </Link>
-
-
-            </ul>
-            <div className='userName'>{complete_name}</div>
-            <div className='myUser'></div>
-
-
-        </div>
-    );
-}
+        <Link to="/Dashboard">
+          <div className="home"></div>
+        </Link>
+      </ul>
+      <div className="userName">{complete_name}</div>
+      <div className="myUser"></div>
+    </div>
+  );
+};
 export default Nav;
 //----------------------------------------
