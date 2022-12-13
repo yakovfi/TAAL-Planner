a7dd4f074279b65524fcc66ee8db669efaf856e9
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-12-13 16:02:17 +0200

    add conditions

--- src/components/TimeData/TimeData.js
+++ src/components/TimeData/TimeData.js
@@ -6,6 +6,7 @@ let result = [];
 const TimeData = () => {
   const [, setResult] = useState([]);
   let arrayTemp = [];
+  let dataResultArray = [];
 
   useEffect(() => {
     (async () => {
@@ -13,6 +14,7 @@ const TimeData = () => {
     })();
   }, []);
 
+  // get: user_id, task_id, route_id, date_range(day, week, month, year, range)
   const gettingData = async () => {
     await get(`https://s83.bfa.myftpupload.com/wp-json/wp/v2/time_data/`, {
       // headers: {
@@ -50,45 +52,59 @@ const TimeData = () => {
 
       console.log("res time data:", res.data);
 
-      // sara levy's Id -should be taken from DB / action
-      const reqId = 20;
+      // sara levy's Id - should be taken from DB / action
+      const reqUserId = 51;
+      const reqTaskId = 20;
+      const currDate = new Date().toJSON().slice(0, 10);
 
-      result = res.data.filter((user) => user.acf.user_id == reqId);
-      // console.log("result: " + result[0].acf.route_title);
-
-      const start_time = result[0].acf.start_time;
-      const end_time = result[0].acf.end_time;
-
-      const diff =
-        new Date(result[58].acf.end_time) - new Date(result[58].acf.start_time);
-      console.log("diff: " + parseFloat(diff / 1000 / 60));
-
-      console.log("start_time: " + start_time);
-      console.log("end_time: " + end_time);
-      // FMT = "%Y-%M-%D %H:%M:%S";
-
-      let secondsCalc = Math.floor(diff / 1000);
-      let minutesCalc = Math.floor(secondsCalc / 60);
-      let hoursCalc = Math.floor(minutesCalc / 60);
-
-      const year = start_time.slice(0, 4);
-      const month = start_time.slice(5, 7);
-      const day = start_time.slice(8, 10);
-      const hours = hoursCalc;
-      const minutes = minutesCalc;
-      const seconds = secondsCalc;
-
-      const resultTimeAndDateOnTask = new Date(
-        year,
-        month - 1,
-        day,
-        hours,
-        minutes,
-        seconds
+      console.log("currDate: " + currDate);
+      result = res.data.filter(
+        (user) =>
+          // example for specific user
+          user.acf.user_id == reqUserId &&
+          // // example for specific task
+          // user.acf.task_id == reqTaskId &&
+          // example for traget time range
+          user.acf.start_time.slice(0, 10) == currDate
       );
 
-      console.log("resultTimeOnTask: " + resultTimeAndDateOnTask);
-      console.log("time on task(seconds): " + diff / 1000);
+      for (let i = 0; i < result.length; i++) {
+        const start_time = result[i].acf.start_time;
+        const end_time = result[i].acf.end_time;
+
+        const diff =
+          new Date(result[i].acf.end_time) - new Date(result[i].acf.start_time);
+
+        console.log("diff in minutes: " + parseFloat(diff / 1000 / 60));
+
+        let secondsCalc = Math.floor(diff / 1000);
+        let minutesCalc = Math.floor(secondsCalc / 60);
+        let hoursCalc = Math.floor(minutesCalc / 60);
+
+        const year = start_time.slice(0, 4);
+        const month = start_time.slice(5, 7);
+        const day = start_time.slice(8, 10);
+        const hours = hoursCalc;
+        const minutes = minutesCalc;
+        const seconds = secondsCalc;
+
+        const resultTimeAndDateOnTask = new Date(
+          year,
+          month - 1,
+          day,
+          hours,
+          minutes,
+          seconds
+        );
+
+        if (i === 1) {
+          dataResultArray = resultTimeAndDateOnTask;
+        } else {
+          Array.prototype.push.apply(dataResultArray, resultTimeAndDateOnTask);
+        }
+      }
+
+      console.log("final result: " + dataResultArray);
     });
   };
   return <div className=""></div>;
